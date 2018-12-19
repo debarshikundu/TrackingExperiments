1676915710a9b3ca9c9f8848a15aaa9097d85f4a
Debarshi Kundu <debarshikundu0@gmail.com>
2018-12-19 03:57:09 -0600

    Still need User File I/O and barriers.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.IO;
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float movementDuration = 2.0f;
+    WaitForSeconds waitBeforeMoving = new WaitForSeconds(2f);
+    Vector3[] path = new Vector3[20];
+
+    void Start()
+    {
+        StartCoroutine(MainRoutine());
+    }
+
+    IEnumerator MainRoutine()
+    {
+        //generate new path:
+        for (int i = 0; i < path.Length; i++)
+        {
+            float timer = 0.0f;
+            Vector3 startPos = transform.position;
+            float x = RandomNum(timer);
+            float y = RandomNum(x);
+            float z = RandomNum(y);
+            path[i] = new Vector3(x, y, z);
+        }
+
+        //traverse path:
+        for (int i = 0; i < path.Length; i++)
+        {
+            float timer = 0.0f;
+            Vector3 startPos = transform.position;
+            while (timer < movementDuration)
+            {
+                timer += Time.deltaTime;
+                float t = timer / movementDuration;
+                t = t * t * t * (t * (6f * t - 15f) + 10f);
+                transform.position = Vector3.Lerp(startPos, path[i], t);
+                yield return null;
+            }
+            yield return waitBeforeMoving;
+        }
+
+        //print path:
+        PrintPoints();
+    }
+
+    void PrintPoints()
+    {
+        string filepath = Application.dataPath + "/Player.txt";
+        if (File.Exists(filepath) == false)
+        {
+            File.WriteAllText(filepath, "The player blob visited these random coordinates: \n\n");
+        }
+        foreach (Vector3 vector in path)
+        {
+            File.AppendAllText(filepath, string.Format("{0}\n\n", JsonUtility.ToJson(vector)));
+        }
+    }
+
+    float RandomNum(float lastRandNum)
+    {
+        //Random value range can be changed in the future if necessary
+        float randNum = Random.Range(-10.0f, 10.0f);
+        return System.Math.Abs(randNum - lastRandNum) < float.Epsilon ? RandomNum(randNum) : randNum;
+    }
+
+}
---
+++ Assets/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshCollider))]
+
+public class UserController : MonoBehaviour {
+
+    public int speed = 20;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // get input data from keyboard or controller
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // update player position based on input
+        Vector3 position = transform.position;
+        position.x += moveHorizontal * speed * Time.deltaTime;
+        position.z += moveVertical * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+void OnMouseDrag()
+    {
+
+        if(Input.GetMouseButton(0))
+        {
+
+                float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+                transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+
+
+           
+        }
+
+      
+
+
+
+    }
+}
+
