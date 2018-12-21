ebda648c19c80d12f424d33a25ae3b7347269bc6
Debarshi Kundu <debarshikundu0@gmail.com>
2018-12-20 23:07:07 -0600

    cleaned up the code

--- Assets/UserController.cs
+++ Assets/UserController.cs
@@ -6,14 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(MeshCollider))]
 
 public class UserController : MonoBehaviour {
-    /*
-    public int speed = 20;
-    static Vector3 m_DistanceFromCamera;
-    public float m_DistanceZ;
-    public GameObject m_Cube;
-    private Plane xy_plane; 
-    private Plane xz_plane;
-    */
+   
 
     bool shiftOn = false;
 
@@ -41,26 +34,16 @@ public class UserController : MonoBehaviour {
 
         }
 
-      
-
-
-
-
-
-    }
+      }
 
     void changeShift()
     {
         shiftOn = !shiftOn;
     }
+
     // Use this for initialization
     void Start()
     {
-        /*
-        m_DistanceFromCamera = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z - m_DistanceZ);
-        xy_plane = new Plane(Vector3.forward, m_DistanceFromCamera);
-        xz_plane = new Plane(Vector3.up, m_DistanceFromCamera);
-        */
 
     }
         // Update is called once per frame
@@ -73,64 +56,14 @@ public class UserController : MonoBehaviour {
             }
 
 
-            /*
-            if (Input.GetMouseButton(0))
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-                //Initialise the enter variable
-                float enter = 0.0f;
-                Vector3 hitPoint = new Vector3();
-                if (xy_plane.Raycast(ray, out enter))
-                {
-                    hitPoint = ray.GetPoint(enter);
-                }
-
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    m_Cube.transform.position = hitPoint;
-                }
-                }
-                */
-
-            //Detect when there is a mouse click
 
 
         }
 
 
 
-        /*
-        // get input data from keyboard or controller
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        // update player position based on input
-        Vector3 position = transform.position;
-        position.x += moveHorizontal * speed * Time.deltaTime;
-        position.z += moveVertical * speed * Time.deltaTime;
-        transform.position = position;
-        */
-
-
     }
 
 
-    /*
-    void OnMouseDrag()
-        {
-
-            if(Input.GetMouseButton(0))
-            {
-
-                    float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-                    transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
-
-
-
-            }
-
-            }
-               */
 
 
